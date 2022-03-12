3c4a0041941239b338f9c8105d053e709da00def
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-03-12 19:43:41 +0300

    Initial Commit

---
+++ SwiftPython/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftPython
+//
+//  Created by Danil Peregorodiev on 12.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftPython/SwiftPythonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPythonApp.swift
+//  SwiftPython
+//
+//  Created by Danil Peregorodiev on 12.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPythonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
